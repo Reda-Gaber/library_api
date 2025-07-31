c3d5c53eb3f62a4e56f4d6e39b40823c6c181aac
= <redagaber.engineer@gmail.com>
2025-07-31 05:03:51 +0300

    Add Cors

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require("express");
+const cors = require("cors");
 const path = require("path");
 const homeRoute = require("./routes/web/home.route")
 const apiBooks = require("./routes/api/books.api")
-const cors = require("cors");
-app.use(cors());
 
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(cors());
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.static(path.join(__dirname, "views")));
